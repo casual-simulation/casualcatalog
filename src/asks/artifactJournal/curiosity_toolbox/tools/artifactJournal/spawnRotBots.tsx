b14decd3eeec822f40cc5a26005812f39bba8619
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-03-10 16:55:18 -0400

    [artifactJournal]: attempting to fix double artifacts and landmarks being created

--- src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/spawnRotBots.tsx
+++ src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/spawnRotBots.tsx
@@ -1,6 +1,13 @@
+if (masks.spawningBots) {
+    return;
+}
+
+masks.spawningBots = true;
 const rotBots = await getBots(byTag("rotBot", true));
 destroy(rotBots);
 
 // await thisBot.spawnMuseumBuildings();
 await thisBot.spawnLandmarks();
 await thisBot.spawnArtifacts();
+
+masks.spawningBots = null;

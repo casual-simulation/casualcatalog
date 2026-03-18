ac5e91a6366ce5bd6285b4edb1a5ffac187676eb
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-03-18 15:42:24 -0400

    [artifactJournal, artifact]: changed the way artifacts spawn in, added a data refresh onInstJoined to check for new content

--- src/asks/artifact/place_toolbox/tools/artifact/showSelf.tsx
+++ src/asks/artifact/place_toolbox/tools/artifact/showSelf.tsx
@@ -3,9 +3,17 @@ const landmarkBot = getBot("landmarkID", that);
 if (tags.collected) {
     return;
 } else {
+    const angle = Math.random() * Math.PI * 2;
+
+    const offsetX = Math.cos(angle) * .0005;
+    const offsetY = Math.sin(angle) * .0005;
+
+    const randomX = landmarkBot.tags.homeX + offsetX;
+    const randomY = landmarkBot.tags.homeY + offsetY;
+
     setTagMask(thisBot, "home", true, "tempLocal");
-    setTagMask(thisBot, "homeX", landmarkBot.tags.homeX + .0005, "tempLocal");
-    setTagMask(thisBot, "homeY", landmarkBot.tags.homeY + .0005, "tempLocal");
+    setTagMask(thisBot, "homeX", randomX, "tempLocal");
+    setTagMask(thisBot, "homeY", randomY, "tempLocal");
     setTagMask(thisBot, "collectable", true, "tempLocal");
     setTagMask(thisBot, "pointable", true, "tempLocal");
-}
+}   
--- src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/onInstJoined.tsx
+++ src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/onInstJoined.tsx
@@ -1,1 +1,2 @@
 thisBot.spawnPlayer();
+thisBot.getDataFromStrapi();

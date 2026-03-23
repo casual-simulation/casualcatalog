ee78098dc0197fafe729516619d6af2342fb997f
Ryan Cook <ryancook423@gmail.com>
2026-03-23 14:15:19 -0400

    [abAction]: moved max history messages count to tag on ask bot.

--- src/ab/abAction/ab/action/ask/abConversationHistorySave.tsx
+++ src/ab/abAction/ab/action/ask/abConversationHistorySave.tsx
@@ -2,10 +2,8 @@ const { historyStorageBot, history } = that ?? {};
 
 assert(ab.links.utils.isBot(historyStorageBot), `[${tags.system}.${tagName}] historyStorageBot must be a Bot.`);
 
-const MAX_HISTORY_MESSAGES = 40;
-
-const trimmed = history.length > MAX_HISTORY_MESSAGES
-    ? [history[0], history[1], ...history.slice(history.length - (MAX_HISTORY_MESSAGES - 2))]
+const trimmed = history.length > tags.maxHistoryMessages
+    ? [history[0], history[1], ...history.slice(history.length - (tags.maxHistoryMessages - 2))]
     : history;
 
 setTagMask(historyStorageBot, 'abConversationHistory', trimmed, historyStorageBot.space);

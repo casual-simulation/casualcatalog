4d5ccc8fff053d6c6eb442721ef5254e94ab0aaf
Ryan Cook <ryancook423@gmail.com>
2026-03-25 14:26:15 -0400

    [abPatchTodo]: moved dimension / grid info back to eggParameters object in artifact.

--- src/asks/abPatchTodo/abPatchTodo/template/onABArtifactCollectShards.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/onABArtifactCollectShards.tsx
@@ -1,15 +1,21 @@
 const shard: ABArtifactShard = {
     data: {
-        dimension: tags.dimension,
-        dimensionX: tags[tags.dimension + 'X'],
-        dimensionY: tags[tags.dimension + 'Y'],
-        dimensionZ: tags[tags.dimension + 'Z'],
         prompt: tags.prompt,
         todoLabel: tags.todoLabel,
         aiModel: tags.aiModel,
         abPatchAskInput: tags.abPatchAskInput,
         alwaysApprove: tags.alwaysApprove,
         debug: tags.debug,
+        eggParameters: {
+            gridInformation: {
+                dimension: tags.dimension,
+                position: {
+                    x: tags[tags.dimension + 'X'] ?? 0,
+                    y: tags[tags.dimension + 'Y'] ?? 0,
+                    z: tags[tags.dimension + 'Z'] ?? 0
+                }
+            }
+        },
     },
     dependencies: [{ askID: 'abPatchTodo '}]
 }
--- src/asks/abPatchTodo/abPatchTodo/template/onABArtifactReconstitute.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/onABArtifactReconstitute.tsx
@@ -7,10 +7,20 @@ tags.todoLabel = data.todoLabel;
 tags.aiModel = data.aiModel;
 tags.debug = data.debug;
 
-tags.dimension = data.dimension ?? 'home';
-tags[dimension + 'X'] = data.dimensionX ?? 0;
-tags[dimension + 'Y'] = data.dimensionY ?? 0;
-tags[dimension + 'Z'] = data.dimensionZ ?? 0;
+if (data.eggParameters) {
+    if (data.eggParameters.gridInformation) {
+        const dimension = data.eggParameters.gridInformation.dimension ?? 'home';
+        const dimensionX = data.eggParameters.gridInformation.position?.x ?? 0;
+        const dimensionY = data.eggParameters.gridInformation.position?.y ?? 0;
+        const dimensionZ = data.eggParameters.gridInformation.position?.z ?? 0;
+
+        tags.dimension = dimension;
+        tags[dimension] = true;
+        tags[dimension + 'X'] = dimensionX;
+        tags[dimension + 'Y'] = dimensionY;
+        tags[dimension + 'Z'] = dimensionZ;
+    }
+}
 
 tags.abPatchTodoInstance = true;
 

6b0b8691b3f3f52eb13f67c5035dcaf230a2e6fe
Ryan Cook <ryancook423@gmail.com>
2026-03-25 15:21:17 -0400

    [abPatchTodo]: load animations before calling first animation

--- src/asks/abPatchTodo/abPatchTodo/template/onABArtifactReconstitute.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/onABArtifactReconstitute.tsx
@@ -24,4 +24,7 @@ if (data.eggParameters) {
 
 tags.abPatchTodoInstance = true;
 
+masks.formAddress = ab.abBuildCasualCatalogURL("/asks/meshes/toDoBot_checklist_animated.glb");
+await os.listFormAnimations(thisBot); // MUST run this before triggering animations. Without it, animations may try to trigger before the animation system is ready.
+
 thisBot.changeAnimationState("incomplete_in");
--- src/asks/abPatchTodo/abPatchTodo/template/onBotAdded.tsx
+++
@@ -1,1 +0,0 @@
-masks.formAddress = ab.abBuildCasualCatalogURL("/asks/meshes/toDoBot_checklist_animated.glb");

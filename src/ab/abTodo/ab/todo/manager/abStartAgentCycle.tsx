06145fafb6bcaa7cccefb94ea0722fab53c26e01
Ryan Cook <ryancook423@gmail.com>
2026-03-27 10:17:16 -0400

    [abTodo]: always execute next todo manager tick even if something goes wrong.

--- src/ab/abTodo/ab/todo/manager/abStartAgentCycle.tsx
+++ src/ab/abTodo/ab/todo/manager/abStartAgentCycle.tsx
@@ -1,14 +1,17 @@
 thisBot.abStopAgentCycle();
 
 async function tick() {
-    await whisper(thisBot, 'onTodoManagerTick')[0];
+    try {
+        await whisper(thisBot, 'onTodoManagerTick')[0];
 
-    if (tags.debug) {
-        console.log(`[${tags.system}.${tagName}] agent tick`);
+        if (tags.debug) {
+            console.log(`[${tags.system}.${tagName}] agent tick`);
+        }
+        shout("onAgentTick");
+    } finally {
+        thisBot.vars.cycleTimeoutId = setTimeout(tick, tags.agentCycleIntervalMS ?? 1000);
     }
-    shout("onAgentTick");
 
-    thisBot.vars.cycleTimeoutId = setTimeout(tick, tags.agentCycleIntervalMS ?? 1000);
 }
 
 thisBot.vars.cycleTimeoutId = setTimeout(tick, 0);

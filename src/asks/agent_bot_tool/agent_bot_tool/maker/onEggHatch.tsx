0c3050628b6e0b9c6fef1a148fc136ed8b3eadd4
Ryan Cook <ryancook423@gmail.com>
2026-03-27 12:47:48 -0400

    [agent_bot_tool]: fixed borked onEggHatch

--- src/asks/agent_bot_tool/agent_bot_tool/maker/onEggHatch.tsx
+++ src/asks/agent_bot_tool/agent_bot_tool/maker/onEggHatch.tsx
@@ -23,6 +23,7 @@ if (that.eggParameters) {
                 dimension,
                 aiModel: match.name,
                 aiProvider: match.provider
+            });
               
 
             openMenu = false;

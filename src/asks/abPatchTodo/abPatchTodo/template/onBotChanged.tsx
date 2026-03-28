e9e0c98fd1e007702918dea63161d4ff84cfb37b
Ryan Cook <ryancook423@gmail.com>
2026-03-27 22:43:20 -0400

    [abPatchTodo] run initialize after a small delay in onBotAdded. Also run intialize when abArtifactShardReconstituted is set.

--- src/asks/abPatchTodo/abPatchTodo/template/onBotChanged.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/onBotChanged.tsx
@@ -1,3 +1,7 @@
+if (that.tags.includes('abArtifactShardReconstituted')) {
+    thisBot.initialize();
+}
+
 if (that.tags.includes('abPatchError') && tags.abPatchError) {
     thisBot.changeAnimationState('error_in');
 }

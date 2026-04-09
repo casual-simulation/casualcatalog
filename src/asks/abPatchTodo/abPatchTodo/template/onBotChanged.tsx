d4714b160548e70ac0587006e5db3fa283d43da1
Ryan Cook <ryancook423@gmail.com>
2026-04-09 10:17:13 -0400

    [abPatchTodo]: added click and drag sounds to abPatchTodo. Added pointer over scaling animation.

--- src/asks/abPatchTodo/abPatchTodo/template/onBotChanged.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/onBotChanged.tsx
@@ -1,7 +1,30 @@
-if (that.tags.includes('abArtifactShardReconstituted')) {
-    thisBot.initialize();
-}
+for (const tag of that.tags) {
+    if (tag === 'abArtifactShardReconstituted') {
+        thisBot.initialize();
+    }
 
-if (that.tags.includes('animationState') && masks.formAddressAnimations) {
-    whisper(thisBot, 'refreshAnimation');
+    if (tag === 'animationState' && masks.formAddressAnimations) {
+        whisper(thisBot, 'refreshAnimation');
+    }
+
+    if (tag === 'pointerOver') {
+        clearAnimations(thisBot, 'scale');
+
+        const maxDuration = 0.125;
+        const overScale = 1.3;
+        const normalScale = 1;
+        const targetScale = tags.pointerOver ? overScale : normalScale;
+        const duration = maxDuration * (Math.abs(tags.scale - targetScale) / (overScale - normalScale));
+        
+        animateTag(thisBot, {
+            fromValue: {
+                scale: tags.scale,
+            },
+            toValue: {
+                scale: tags.pointerOver ? overScale : normalScale,
+            },
+            easing: 'sinusoidal',
+            duration
+        }).catch(() => {});
+    }
 }

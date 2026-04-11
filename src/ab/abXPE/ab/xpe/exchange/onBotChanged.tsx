9d902d5a4e11f436acee30675a76b9fdda8a65b4
Ryan Cook <ryancook423@gmail.com>
2026-04-11 19:27:01 -0400

    [abXPE} remove debug log

--- src/ab/abXPE/ab/xpe/exchange/onBotChanged.tsx
+++ src/ab/abXPE/ab/xpe/exchange/onBotChanged.tsx
@@ -1,5 +1,4 @@
 if (that.tags.includes('availableCredits')) {
-    console.log(`available credits changed`, masks.availableCredits);
     whisper(thisBot, 'onABXPEAvailableCreditsChanged');
 }
 if (that.tags.includes('creditsRefreshTimeoutMS')) {

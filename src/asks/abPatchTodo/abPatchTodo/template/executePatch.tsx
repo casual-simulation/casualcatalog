761120b3485dde81b07eef3717ac8a9f435b5016
Ryan Cook <ryancook423@gmail.com>
2026-04-11 21:10:31 -0400

    [abPatchTodo]: no longer automatically open a todo bots menu when executing its patch code.

--- src/asks/abPatchTodo/abPatchTodo/template/executePatch.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/executePatch.tsx
@@ -1,5 +1,3 @@
 await thisBot.abPatchApply();
 
 await os.sleep(0); // NOTE: Need to wait 1 frame and let tags/masks updates in the patch bots otherwise the menu wont be able to retrieve all patch bots successfully.
-
-thisBot.abPatchTodoMenuOpen();

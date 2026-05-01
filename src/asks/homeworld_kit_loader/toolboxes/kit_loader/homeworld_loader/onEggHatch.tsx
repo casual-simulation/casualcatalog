a49269f5d10fdb0727cf881cbc10e777f188d54f
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-05-01 17:45:23 -0400

    [kit_loaders]: made kit loaders for all existing kits

---
+++ src/asks/homeworld_kit_loader/toolboxes/kit_loader/homeworld_loader/onEggHatch.tsx
@@ -0,0 +1,36 @@
+if (that.eggParameters) {
+    const dimension = that.eggParameters.gridInformation?.dimension ?? 'home';
+    const dimensionX = that.eggParameters.gridInformation?.position?.x ?? 0;
+    const dimensionY = that.eggParameters.gridInformation?.position?.y ?? 0;
+
+    const abArtifactShard = {
+        data: {
+            label: that.eggParameters.toolbox_name ?? "homeworld kit",
+            studioId: that.eggParameters.studioId,
+            tool_array: tags.tool_array,
+            eggParameters: {
+                gridInformation: {
+                    dimension: dimension,
+                    position: {
+                        x: dimensionX,
+                        y: dimensionY
+                    }
+                }
+            }
+        },
+        dependencies: [
+            {
+                askID: 'kit'
+            }
+        ]
+    };
+    ab.links.artifact.abCreateArtifactPromiseBot({
+        abArtifactName: 'kit',
+        abArtifactInstanceID: uuid(),
+        abArtifactShard,
+    });
+}
+
+if (!configBot.tags.kitLoaderDevMode) {
+    destroy(thisBot);
+}

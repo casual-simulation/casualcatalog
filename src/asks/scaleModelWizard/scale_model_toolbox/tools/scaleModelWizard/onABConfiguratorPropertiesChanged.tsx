c4180d129bad7ff1380af3fc0c33f455a5527b01
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-05-04 17:50:16 -0400

    [scaleModelWizard]: toggling off using todos until I can fix it.

--- src/asks/scaleModelWizard/scale_model_toolbox/tools/scaleModelWizard/onABConfiguratorPropertiesChanged.tsx
+++ src/asks/scaleModelWizard/scale_model_toolbox/tools/scaleModelWizard/onABConfiguratorPropertiesChanged.tsx
@@ -4,7 +4,7 @@ const { propertyValues } = that;
 if ('prompt' in propertyValues) {
     tags.userPrompt = propertyValues.prompt;
     if (propertyValues.prompt) {
-       //thisBot.generatePlan(propertyValues.prompt);
-       thisBot.useTodoPlan(propertyValues.prompt);
+       thisBot.generatePlan(propertyValues.prompt);
+       //thisBot.useTodoPlan(propertyValues.prompt);
     }
 }

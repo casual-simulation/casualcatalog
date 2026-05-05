47d01877791964e4a06ad770afecbca4911e6e34
Ryan Cook <ryancook423@gmail.com>
2026-05-05 11:08:07 -0400

    [studioCatalog]: hide kits that are already loaded from this catalog. show 'all kits loaded' message when placing catalog arm after all kits are loaded.

--- src/asks/studioCatalog/place_toolbox/tools/studioCatalog/handleToolMenu.tsx
+++ src/asks/studioCatalog/place_toolbox/tools/studioCatalog/handleToolMenu.tsx
@@ -1,58 +1,80 @@
 const gridInformation = {dimension: that?.dimension, position: { x: that?.x, y: that?.y}} ?? abRemember.tags.abGridFocus;
 const toolboxes = [...tags.toolbox_array] ?? [];
 
+// Strip out toolboxes that are alreacy loaded for this studio.
+for (let i = toolboxes.length - 1; i >= 0; i--) {
+    const existingToolboxBot = getBot((b) => {
+        return b.tags.abArtifactName === 'kit' && 
+               b.tags.studioId === tags.studioId &&
+               b.tags.label === toolboxes[i].title
+    });
+
+    if (existingToolboxBot) {
+        toolboxes.splice(i, 1);
+    }
+}
+
+
 await os.sleep(0);
 
-const menuOptions = {};
+const BASE_MENU_TAGS = {
+    dimension: 'studioCatalog_toolMenu',
+    studioCatalog_toolMenu: true,
+    abMenuRefresh: '@destroy(thisBot)',
+    clearStudioStationToolMenu: `@destroy(thisBot)`,
+    toolbox: getLink(thisBot),
+    gridInformation: gridInformation,
+    search: tags.search,
+    artifact: tags.artifact,
+}
 
-shout("clearStudioStationToolMenu");
+shout('clearStudioStationToolMenu');
 configBot.masks.menuPortal = null;
 configBot.tags.menuPortal = 'studioCatalog_toolMenu';
 
-menuOptions.dimension = 'studioCatalog_toolMenu';
-menuOptions['studioCatalog_toolMenu'] = true;
-menuOptions.abMenuRefresh = "@ destroy(thisBot);";
-menuOptions.clearStudioStationToolMenu = `@destroy(thisBot);`;
-menuOptions.toolbox = getLink(thisBot);
-menuOptions.gridInformation = gridInformation;
-menuOptions.search = tags.search;
-menuOptions.artifact = tags.artifact;
-
-const toolboxesGroup = {
-    ...menuOptions,
-    studioCatalog_toolMenuSortOrder: 2,
-    groupSortOrder: 2,
-    menuItems: []
-}
-
-for (let toolbox of toolboxes) {
-    const toolboxButton = {
-        ...menuOptions,
-        label: toolbox.title,
-        toolboxTitle: toolbox.title,
-        studioId: tags.studioId,
-        formAddress: 'home_repair_service',
-        targetAB: toolbox.name,
-        onClick:`@
-            await ab.links.search.onLookupAskID({
-            askID: tags.targetAB,
-            eggParameters: {
-                studioId: tags.studioId,
-                gridInformation: {
-                    toolbox_name: tags.toolboxTitle,
-                    ...tags.gridInformation
-                }
-            }
-        })
-        shout("abMenuRefresh");
-        `,
-    };
-
-    if (toolbox.formAddress) {
-        toolButton.formAddress = toolbox.formAddress;
+if (toolboxes.length > 0) {
+    const toolboxesGroup = {
+        ...BASE_MENU_TAGS,
+        studioCatalog_toolMenuSortOrder: 2,
+        groupSortOrder: 2,
+        menuItems: []
     }
 
-    toolboxesGroup.menuItems.push(toolboxButton);
-}
+    for (let toolbox of toolboxes) {
+        const toolboxButton = {
+            ...BASE_MENU_TAGS,
+            label: toolbox.title,
+            toolboxTitle: toolbox.title,
+            studioId: tags.studioId,
+            formAddress: 'home_repair_service',
+            targetAB: toolbox.name,
+            onClick: ListenerString(async () => {
+                await ab.links.search.onLookupAskID({
+                    askID: tags.targetAB,
+                    eggParameters: {
+                        studioId: tags.studioId,
+                        gridInformation: {
+                            toolbox_name: tags.toolboxTitle,
+                            ...tags.gridInformation
+                        }
+                    }
+                })
 
-ab.links.menu.abCreateMenuGroup(toolboxesGroup);
+                shout('abMenuRefresh');
+            })
+        };
+
+        if (toolbox.formAddress) {
+            toolButton.formAddress = toolbox.formAddress;
+        }
+
+        toolboxesGroup.menuItems.push(toolboxButton);
+    }
+
+    ab.links.menu.abCreateMenuGroup(toolboxesGroup);
+} else {
+    ab.links.menu.abCreateMenuText({
+        ...BASE_MENU_TAGS,
+        label: `all kits have been loaded from ${tags.label}`
+    })
+}

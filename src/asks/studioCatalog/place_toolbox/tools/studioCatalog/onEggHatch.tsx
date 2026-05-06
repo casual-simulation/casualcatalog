421ffcfc072d49cf392c3f4f97d469fe2d76398e
Ryan Cook <ryancook423@gmail.com>
2026-05-06 09:32:41 -0400

    [kit, studioCatalog]: merging onABArtifactReconstitute and onEggHatch for studioCatalog so that it gets setup properly no matter how its created. Changed 'onStudioCatalogReconstituted' shout to 'onStudioCatalogSetup'.

--- src/asks/studioCatalog/place_toolbox/tools/studioCatalog/onEggHatch.tsx
+++ src/asks/studioCatalog/place_toolbox/tools/studioCatalog/onEggHatch.tsx
@@ -1,1 +1,3 @@
 console.log(`[${tags.system}.${tagName}] that:`, that);
+
+thisBot.setup({ data: that.eggParameters });
---
+++ src/asks/studioCatalog/place_toolbox/tools/studioCatalog/setup.tsx
@@ -0,0 +1,61 @@
+const data = that.data ?? {};
+
+tags.label = data.label ?? 'catalog';
+tags.scaleX = data.scaleX ?? 2;
+tags.scaleY = data.scaleY ?? 2;
+tags.scaleZ = data.scaleZ ?? 1;
+tags.color = data.color ?? abPersonality?.tags?.abBaseColor ?? '#00D9CD';
+tags.labelFloatingBackgroundColor = data.labelFloatingBackgroundColor ?? abPersonality?.tags?.abBaseColor ?? '#00D9CD'; 
+tags.labelColor = data.labelColor ?? abPersonality?.tags?.abBaseLabelColor ?? 'black';
+tags.studioId = data.studioId;
+tags.strokeColor = abPersonality?.tags.abBaseStrokeColor;
+tags.prevBotID = data.prevBotID;
+tags.respawnPoint = data.respawnPoint;
+tags.toolbox_array = data.toolbox_array ?? ab.links.remember.tags.toolbox_array;
+tags.armColor = abPersonality?.tags.abBaseStrokeColor;
+tags.armMeshPath = ab.links.remember.tags.abArmMeshPath;
+tags.abIgnore = true;
+
+if (data.dimensionData) {
+    for (const tagName in data.dimensionData) {
+        tags[tagName] = data.dimensionData[tagName];
+    }
+}
+
+if (data.eggParameters) {
+    ab.links.utils.applyGridInfoFromEggParams({ bot: thisBot, eggParameters: data.eggParameters });
+}
+
+let hasCustomMesh = false;
+if (tags.studioId) {
+    hasCustomMesh = await thisBot.applyStudioConfig();
+}
+
+if (!hasCustomMesh) {
+    tags.formAddress = ab.abBuildCasualCatalogURL('/asks/meshes/hexagon.glb');
+    tags.strokeFormAddress = ab.abBuildCasualCatalogURL('/asks/meshes/hexagon_stroke.glb');
+    const strokeBot = await thisBot.generateStroke();
+    tags.strokeBot = getLink(strokeBot);
+}
+
+if (!tags.studioId && authBot) {
+    if (!configBot.tags.user_studios) {
+        await ab.abRefreshStudios();
+    }
+    
+    // If there the user is not part of any studios, automatically load this catalog with the user studio.
+    const studios = configBot.tags.user_studios?.studios ?? [];
+    if (studios.length === 0) {
+        thisBot.setStudio({
+            studioId: authBot?.id,
+            displayName: 'user studio'
+        })
+    }
+}
+
+shout('onStudioCatalogSetup', thisBot);
+
+if (!tags.studioId) {
+    // If the bot doesn't have a studioId, automatically open the studio select menu.
+    thisBot.onClick();
+}

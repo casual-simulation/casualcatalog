1af56b000a8d1dff706b3ebdaedae9787c43d31d
Ryan Cook <ryancook423@gmail.com>
2026-05-11 19:28:48 -0400

    [abCore]: debug log abIsPrimary.

--- src/ab/abCore/ab/core/learn/abIsPrimary.tsx
+++ src/ab/abCore/ab/core/learn/abIsPrimary.tsx
@@ -1,4 +1,6 @@
 const curInst = os.getCurrentInst();
 const primaryInst = thisBot.abPrimaryInst();
 
+console.log(`[${tags.system}.${tagName}] curInst: ${curInst}, primaryInst: ${primaryInst}, abIsPrimary: ${curInst === primaryInst}`);
+
 return curInst === primaryInst;

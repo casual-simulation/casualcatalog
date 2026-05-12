0e64fe39c923c5e73f6d9b31203831966b672bfa
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-05-12 16:10:42 -0400

    [artifactJournal]: fixing pagination issue causing artifact data to not appear

--- src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/getDataFromStrapi.tsx
+++ src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/getDataFromStrapi.tsx
@@ -7,7 +7,7 @@ try {
     //collectables
     const collectablesResponse = await web.hook({
         method: 'GET',
-        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/collectables?populate=*',
+        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/collectables?pagination[start]=0&pagination[limit]=1000&populate=*',
         headers:
         {
             "Authorization": "Bearer 86b20df42323b5bceb010a3274238da52afa06cd3267713f4e284bdf0c3a1acb7ea91f59390203c529456ed7ae594ef1654267a2e32a52f19c1c7f10df0d640cf8e4b0d6821b691b9df9ae79e43575e738071ab2fb244bfa2c957090acee283e73428faeb67342bec3f4277f717d6c7cc6cd6719103997c85cf1e2a071365e07"
@@ -38,7 +38,7 @@ try {
     //artifact
     const artifactResponse = await web.hook({
         method: 'GET',
-        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/artifacts',
+        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/artifacts?pagination[start]=0&pagination[limit]=1000',
         headers:
         {
             "Authorization": "Bearer 86b20df42323b5bceb010a3274238da52afa06cd3267713f4e284bdf0c3a1acb7ea91f59390203c529456ed7ae594ef1654267a2e32a52f19c1c7f10df0d640cf8e4b0d6821b691b9df9ae79e43575e738071ab2fb244bfa2c957090acee283e73428faeb67342bec3f4277f717d6c7cc6cd6719103997c85cf1e2a071365e07"
@@ -59,7 +59,7 @@ try {
     //landmark
     const landmarkResponse = await web.hook({
         method: 'GET',
-        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/landmarks',
+        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/landmarks?pagination[start]=0&pagination[limit]=1000',
         headers:
         {
             "Authorization": "Bearer 86b20df42323b5bceb010a3274238da52afa06cd3267713f4e284bdf0c3a1acb7ea91f59390203c529456ed7ae594ef1654267a2e32a52f19c1c7f10df0d640cf8e4b0d6821b691b9df9ae79e43575e738071ab2fb244bfa2c957090acee283e73428faeb67342bec3f4277f717d6c7cc6cd6719103997c85cf1e2a071365e07"
@@ -70,7 +70,7 @@ try {
     //discoverable
     const discoverableResponse = await web.hook({
         method: 'GET',
-        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/discoverables?pagination[start]=0&pagination[limit]=100',
+        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/discoverables?pagination[start]=0&pagination[limit]=1000',
         headers:
         {
             "Authorization": "Bearer 86b20df42323b5bceb010a3274238da52afa06cd3267713f4e284bdf0c3a1acb7ea91f59390203c529456ed7ae594ef1654267a2e32a52f19c1c7f10df0d640cf8e4b0d6821b691b9df9ae79e43575e738071ab2fb244bfa2c957090acee283e73428faeb67342bec3f4277f717d6c7cc6cd6719103997c85cf1e2a071365e07"
@@ -81,7 +81,7 @@ try {
     //collections
     const collectionsResponse = await web.hook({
         method: 'GET',
-        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/collections',
+        url: 'https://generous-eggs-66ccb5f42c.strapiapp.com/api/collections?pagination[start]=0&pagination[limit]=1000',
         headers: 
         {
             "Authorization":"Bearer 86b20df42323b5bceb010a3274238da52afa06cd3267713f4e284bdf0c3a1acb7ea91f59390203c529456ed7ae594ef1654267a2e32a52f19c1c7f10df0d640cf8e4b0d6821b691b9df9ae79e43575e738071ab2fb244bfa2c957090acee283e73428faeb67342bec3f4277f717d6c7cc6cd6719103997c85cf1e2a071365e07" 

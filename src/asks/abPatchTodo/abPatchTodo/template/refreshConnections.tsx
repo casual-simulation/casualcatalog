c561424df9cd0fe08a98c26c0823d2389a1a6dce
Ryan Cook <ryancook423@gmail.com>
2026-05-15 11:53:53 -0400

    [abPatchTodo]: fixed bad reference to todoParentId tag.

--- src/asks/abPatchTodo/abPatchTodo/template/refreshConnections.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/refreshConnections.tsx
@@ -17,7 +17,7 @@ if (nextTodo) {
 
 // First child todo in a plan should always draw a connection line to its parent.
 if (tags.todoParentId && (tags.todoOrder ?? 0) === 0) {
-    const todoParentBot = getBot('id', todoParentId);
+    const todoParentBot = getBot('id', tags.todoParentId);
     if (todoParentBot) {
         lineToSet.push(todoParentBot);
     }

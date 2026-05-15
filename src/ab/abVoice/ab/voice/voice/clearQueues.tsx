3ef8fdf4985a195267721663b93f81bf3ffb995c
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-05-15 12:02:38 -0400

    [abVoice]: fixing error where audio/text queues wouldnt be fully cleared out on user interruption

--- src/ab/abVoice/ab/voice/voice/clearQueues.tsx
+++ src/ab/abVoice/ab/voice/voice/clearQueues.tsx
@@ -1,5 +1,5 @@
-thisBot.masks.audioQueue && thisBot.masks.audioQueue.length > 0 ? thisBot.masks.audioQueue.length = 1 : null;
-thisBot.masks.textQueue && thisBot.masks.textQueue.length > 0 ? thisBot.masks.textQueue.length = 1 : null;
+thisBot.masks.audioQueue && thisBot.masks.audioQueue.length > 0 ? thisBot.masks.audioQueue = [] : null;
+thisBot.masks.textQueue && thisBot.masks.textQueue.length > 0 ? thisBot.masks.textQueue = [] : null;
 
 os.cancelSound(thisBot.masks.activeSound);
 

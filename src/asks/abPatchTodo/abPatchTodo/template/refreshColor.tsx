320216a10f93748d2177bac053c25be35a71c568
Ryan Cook <ryancook423@gmail.com>
2026-05-20 15:52:40 -0400

    [abPatchTodo]: dont color todo bots, only the arrows for now.

--- src/asks/abPatchTodo/abPatchTodo/template/refreshColor.tsx
+++ src/asks/abPatchTodo/abPatchTodo/template/refreshColor.tsx
@@ -1,4 +1,5 @@
-tags.color = tags.todoBaseColor;
+// [Ryan] Don't change the color of the todo bots, only the arrows at the moment.
+// tags.color = tags.todoBaseColor;
 
 if (links.arrowBot) {
     setTag(links.arrowBot, 'color', tags.todoBaseColor);

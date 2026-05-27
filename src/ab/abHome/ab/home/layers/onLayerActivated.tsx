2b1dd30b10523cb52722950fc385054e9c8f4a3e
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-05-27 16:51:33 -0400

    [abHome]: more iteration.

--- src/ab/abHome/ab/home/layers/onLayerActivated.tsx
+++ src/ab/abHome/ab/home/layers/onLayerActivated.tsx
@@ -1,17 +1,17 @@
 superShout("superLog", `onLayerActivated, ${JSON.stringify(that)}, ${os.getCurrentInst()}, ${links.learn.abIsPrimary()}`);
 
-if (!links.learn.abIsPrimary()) {
+if (links.learn.abIsPrimary()) {
+    if (!tags.activeInsts) {
+        setTagMask(thisBot, "activeInsts", []);
+    }
+
+    console.log("layerActive", JSON.stringify(that))
+    if (!tags.activeInsts.includes(that)) {
+        masks.activeInsts.push(that);
+
+        thisBot.updateLayersMenuDropdown();
+    }
+} else {
     superShout("getLayerLoadStudio", os.getCurrentInst());
-    return;
 }
 
-if (!tags.activeInsts) {
-    setTagMask(thisBot, "activeInsts", []);
-}
-
-console.log("layerActive", JSON.stringify(that))
-if (!tags.activeInsts.includes(that)) {
-    masks.activeInsts.push(that);
-
-    thisBot.updateLayersMenuDropdown();
-}

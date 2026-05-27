d54d519f647cd5a2614899acc114bda28e64c289
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-05-27 10:51:13 -0400

    [landmark]: changing default color of the landmarks, updating octahedron glb

--- src/asks/landmark/place_toolbox/tools/landmark/setStatusVisuals.tsx
+++ src/asks/landmark/place_toolbox/tools/landmark/setStatusVisuals.tsx
@@ -3,7 +3,7 @@ if (tags.discovered) {
     tags.labelFloatingBackgroundColor = '#4BB7E6';
     tags.label = tags.landmarkName;
 } else {
-    tags.color = '#a1a1a1';
-    tags.labelFloatingBackgroundColor = '#a1a1a1';
+    tags.color = '#FFDE28';
+    tags.labelFloatingBackgroundColor = '#FFDE28';
     tags.label = null;
 }

c48c7595a760cfef5002ff64a7a93ad3caf64b51
Shane <shane@yeticgi.com>
2026-06-08 12:18:06 -0400

    [Curiosity Camera] Update Cam to check for embeded teachable machine url to use

--- src/asks/curiosityCamera/curiosity_toolbox/tools/curiosityCamera/useTeachMac.tsx
+++ src/asks/curiosityCamera/curiosity_toolbox/tools/curiosityCamera/useTeachMac.tsx
@@ -1,4 +1,24 @@
+
+let defaultMachineURL = "https://teachablemachine.withgoogle.com/models/dBJN4H14W/";
+let overrideURL = "";
+
+const urlObj = new URL(configBot.tags.url);
+const teachableParam = urlObj.searchParams.get("tmurl"); 
+
+if(teachableParam && teachableParam != ""){
+	overrideURL = teachableParam;
+}
+
+if(overrideURL && overrideURL != ""){
+    defaultMachineURL = overrideURL;
+}
+
 await os.openImageClassifier({
-    modelUrl: "https://teachablemachine.withgoogle.com/models/dBJN4H14W/",
+    modelUrl: defaultMachineURL,
     cameraType: 'rear'
 });
+
+// await os.openImageClassifier({
+//     modelUrl: "https://teachablemachine.withgoogle.com/models/dBJN4H14W/",
+//     cameraType: 'rear'
+// });

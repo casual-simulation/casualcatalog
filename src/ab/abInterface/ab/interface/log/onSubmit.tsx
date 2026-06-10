477ee909082ce89d653dfddb09573ae912a984e1
Ryan Cook <ryancook423@gmail.com>
2026-06-10 12:17:28 -0400

    [abInterface, abShell]: text entered through the "log" input are added to the user log record.

--- src/ab/abInterface/ab/interface/log/onSubmit.tsx
+++ src/ab/abInterface/ab/interface/log/onSubmit.tsx
@@ -7,8 +7,15 @@ if (!ab.links.console.masks.open) {
 
 const username = await ab.links.console.getUserName({ canSetPreferredName: true });
 
+let writeToLogRecord = true;
+
 if (that.text && that.text[0] == ".") {
     ab.links.input.onChat({message: that.text});
+    writeToLogRecord = false;
 }
 
 ab.log({message: that.text, name: username, space: "shared"});
+
+if (writeToLogRecord) {
+    ab.links.log_record.abWriteToLogRecord({ content: that.text });
+}

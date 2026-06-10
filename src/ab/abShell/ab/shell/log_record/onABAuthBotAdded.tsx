7e9d130ae9e29aac5d2d524f63a2cf43e26f10fd
Ryan Cook <ryancook423@gmail.com>
2026-06-10 12:26:01 -0400

    [abShell] log_record + opps: make sure user log record and user opps record are created after ab is initialized as well as when the auth bot gets added to the inst (signaling that the user has logged in).

---
+++ src/ab/abShell/ab/shell/log_record/onABAuthBotAdded.tsx
@@ -0,0 +1,4 @@
+if (masks.abInitialized) { 
+    // Only initialize if authBot is added after ab was initialized.
+    thisBot.initialize();
+}
--- src/ab/abShell/ab/shell/opps/initialize.tsx
+++ src/ab/abShell/ab/shell/opps/initialize.tsx
@@ -2,4 +2,18 @@ if (masks.initialized) {
     return;
 }
 
+const userRecordId = await ab.links.utils.getLocalUserRecordId();
+
+if (userRecordId) {
+    const created = await thisBot.abCreateOppsRecord();
+
+    if (tags.debug) {
+        console.log(`[${tags.system}.${tagName}] created opps log record?`, created);
+    }
+} else {
+    if (tags.debug) {
+        console.log(`[${tags.system}.${tagName}] user is not logged in.`);
+    }
+}
+
 masks.initialized = true;

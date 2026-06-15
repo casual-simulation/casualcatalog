112b648a74187a3074490e32c11be38dc74f91ff
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-06-15 15:34:45 -0400

    [abInterface, studioCatalog]: iterating on equipped catalogs

--- src/asks/studioCatalog/place_toolbox/tools/studioCatalog/abEquipmentCustomDeselect.tsx
+++ src/asks/studioCatalog/place_toolbox/tools/studioCatalog/abEquipmentCustomDeselect.tsx
@@ -2,5 +2,6 @@ os.stopFormAnimation(links.defaultVisualBot);
 ab.links.equipment.onEquipmentBaseDeselected(thisBot);
 
 if (tags.hasCustomMesh || tags.currentFormAnimation == 'closing' || tags.currentFormAnimation == 'closed') {
+    await os.sleep(1000);
     thisBot.hideCatalog();
 }

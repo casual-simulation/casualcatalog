ddab1af3350819cf8469f71276141bf4f248006f
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-06-16 18:29:05 -0400

    [placePortalInitializer]: removing extra catalog spawn, changing avatar to tempShared, moving avatar respawn logic to onAbinitialized

--- src/asks/placePortalInitializer/casual_toolbox/tools/placePortalInitializer/onABInitialized.tsx
+++ src/asks/placePortalInitializer/casual_toolbox/tools/placePortalInitializer/onABInitialized.tsx
@@ -1,1 +1,35 @@
 gridPortalBot.tags.portalColor = configBot.tags.portalColor ?? abPersonality.tags.abBaseColor ?? null;
+
+//add avatar
+const avatarBot = getBot(byTag("mapAvatar", true), byTag("ownerID", authBot?.id));
+
+if (!avatarBot) {
+    //get user location if applicable
+    let posX = 5;
+    let posY = 5;
+
+    const abArtifactShard = {
+        data: {
+            eggParameters: {
+                gridInformation: {
+                    dimension: 'home',
+                    position: {
+                        x: posX,
+                        y: posY
+                    }
+                }
+            }
+        },
+        dependencies: [
+            {
+                askID: 'mapAvatar'
+            }
+        ]
+    };
+    await ab.links.artifact.abCreateArtifactPromiseBot({
+        space: 'tempShared',
+        abArtifactName: 'mapAvatar',
+        abArtifactInstanceID: uuid(),
+        abArtifactShard,
+    });
+}
--- src/asks/placePortalInitializer/casual_toolbox/tools/placePortalInitializer/setupPlacePortal.tsx
+++ src/asks/placePortalInitializer/casual_toolbox/tools/placePortalInitializer/setupPlacePortal.tsx
@@ -4,74 +4,41 @@
 
 //add back button
 
-//add avatar
-const avatarBot = getBot(byTag("mapAvatar", true), byTag("ownerID", authBot?.id));
+// //add avatar
+// const avatarBot = getBot(byTag("mapAvatar", true), byTag("ownerID", authBot?.id));
 
-if (!avatarBot) {
-    //get user location if applicable
-    let posX = 5;
-    let posY = 5;
+// if (!avatarBot) {
+//     //get user location if applicable
+//     let posX = 5;
+//     let posY = 5;
 
-    const abArtifactShard = {
-        data: {
-            eggParameters: {
-                gridInformation: {
-                    dimension: 'home',
-                    position: {
-                        x: posX,
-                        y: posY
-                    }
-                }
-            }
-        },
-        dependencies: [
-            {
-                askID: 'mapAvatar'
-            }
-        ]
-    };
-    await ab.links.artifact.abCreateArtifactPromiseBot({
-        abArtifactName: 'mapAvatar',
-        abArtifactInstanceID: uuid(),
-        abArtifactShard,
-    });
-}
+//     const abArtifactShard = {
+//         data: {
+//             eggParameters: {
+//                 gridInformation: {
+//                     dimension: 'home',
+//                     position: {
+//                         x: posX,
+//                         y: posY
+//                     }
+//                 }
+//             }
+//         },
+//         dependencies: [
+//             {
+//                 askID: 'mapAvatar'
+//             }
+//         ]
+//     };
+//     await ab.links.artifact.abCreateArtifactPromiseBot({
+//         space: 'tempShared',
+//         abArtifactName: 'mapAvatar',
+//         abArtifactInstanceID: uuid(),
+//         abArtifactShard,
+//     });
+// }
 
-const catalog = getBot(byTag("studioCatalog", true), byTag("studioId", authBot?.id));
-
-if (!catalog) {
-    //add catalog
-    const userAbArtifactShard = {
-        data: {
-            studioId: authBot.id,
-            label: 'user studio catalog',
-            autoLoadCasualKit: true,
-            eggParameters: {
-                toolboxBot: null,
-                gridInformation: {
-                    dimension: 'home',
-                    position: {
-                        x: 2,
-                        y: 2
-                    }
-                }
-            }
-        },
-        dependencies: [
-            {
-                askID: 'studioCatalog'
-            }
-        ]
-    };
-    await ab.links.artifact.abCreateArtifactPromiseBot({
-        abArtifactName: 'studioCatalog',
-        abArtifactInstanceID: uuid(),
-        abArtifactShard: userAbArtifactShard,
-        space: 'local',
-    });
-}
-
-gridPortalBot.tags.portalColor = configBot.tags.portalColor ?? abPersonality.tags.abBaseColor ?? null;
+// gridPortalBot.tags.portalColor = configBot.tags.portalColor ?? abPersonality.tags.abBaseColor ?? null;
 
 const grid = create({
     color: 'white',

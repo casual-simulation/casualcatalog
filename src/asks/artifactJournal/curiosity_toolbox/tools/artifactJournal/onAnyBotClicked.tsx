0df327743c00d1c7876e29d93b7fdcf0aaaf104a
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-06-16 17:53:03 -0400

    [artifactJournal]: fixing issue causing artifacts to not show info when clicked

--- src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/onAnyBotClicked.tsx
+++ src/asks/artifactJournal/curiosity_toolbox/tools/artifactJournal/onAnyBotClicked.tsx
@@ -1,4 +1,4 @@
-if (tags.currentRegisteredApp && that.dimension != configBot.tags.menuPortal && that.bot != thisBot && configBot.tags.menuPortal != 'rotJournal_menu') {
+if (tags.currentRegisteredApp && that.dimension != configBot.tags.menuPortal && that.bot != thisBot && configBot.tags.menuPortal != 'rotJournal_menu' && !that.bot.tags.rotArtifact) {
     os.unregisterApp(tags.currentRegisteredApp);
     tags.currentRegisteredApp = null;
 } 

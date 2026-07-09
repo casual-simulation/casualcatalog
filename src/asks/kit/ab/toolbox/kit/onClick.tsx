18bf0fd83f4ff6a157d6b608afc150870bdcbc31
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-07-09 15:02:57 -0400

    [kit]: added onClick to place arm

--- src/asks/kit/ab/toolbox/kit/onClick.tsx
+++ src/asks/kit/ab/toolbox/kit/onClick.tsx
@@ -4,14 +4,28 @@ if (that) {
     }
 }
 
-shout('abMenuRefresh');
-shout("clearKitMenu");
+// shout('abMenuRefresh');
+// shout("clearKitMenu");
 
-configBot.tags.menuPortal = 'kit_menu';
+// configBot.tags.menuPortal = 'kit_menu';
 
-const menuOptions = {
-    kit_menu: true,
-    clearKitMenu: `@destroy(thisBot);`,
-    abMenuRefresh: "@destroy(thisBot);",
-    kit: getLink(thisBot)
-}
+// const menuOptions = {
+//     kit_menu: true,
+//     clearKitMenu: `@destroy(thisBot);`,
+//     abMenuRefresh: "@destroy(thisBot);",
+//     kit: getLink(thisBot)
+// }
+
+//create arm
+const dimension = configBot.tags.mapPortal ?? configBot.tags.gridPortal;
+const inMap = configBot.tags.mapPortal ? true: false;
+const armBot = ab.links.arm_tool.abCreateArm({
+    originBot: thisBot,
+    dimension: dimension,
+    position: {
+        x: tags[dimension + 'X'] + (inMap ? 0.0002 : 2),
+        y: tags[dimension + 'Y']
+    },
+})
+
+thisBot.onArmPlaced({'dimension': dimension, 'x': tags[dimension + 'X'] + (inMap ? 0.0002 : 2), 'y': tags[dimension + 'Y']})

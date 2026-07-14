edafd078f72cbb0a1968495711afaf5a509f6521
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-07-14 14:34:27 -0400

    [abHome]: adjusting where the respawn code happens

--- src/ab/abHome/ab/home/homeworld/onABInitialized.tsx
+++ src/ab/abHome/ab/home/homeworld/onABInitialized.tsx
@@ -3,6 +3,15 @@ configBot.tags.abStayAwake = null;
 os.syncConfigBotTagsToURL(["abStayAwake"]);
 
 if (links.learn.abIsPrimary()) {
+    if (!tags.homeRespawnX) {
+        const respawnData = await os.getData(studio, "homeworldRespawnPoint");
+        console.log("respawn", respawnData)
+        if (respawnData) {
+            masks.homeRespawnX = respawnData.x;
+            masks.homeRespawnY = respawnData.y;
+        }
+    }
+
     if (!tags.introPlayed) {
         setTagMask(links.remember, "mapPreventFocus", true);
         thisBot.init();

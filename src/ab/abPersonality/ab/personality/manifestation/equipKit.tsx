08134231e3e4f4550864ea960234760494630283
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-07-31 12:48:30 -0400

    [abInterface]: randomizing spit out angle for kits, moving ab to the absorbed kit location

--- src/ab/abPersonality/ab/personality/manifestation/equipKit.tsx
+++ src/ab/abPersonality/ab/personality/manifestation/equipKit.tsx
@@ -1,6 +1,8 @@
 const dimension = configBot.tags.mapPortal ?? configBot.tags.gridPortal;
 const posX = links.abBot?.tags[dimension + 'X'];
 const posY = links.abBot?.tags[dimension + 'Y'];
+let newPosX = posX;
+let newPosY = posY;
 
 const inMap = configBot.tags.mapPortal ? true : false;
 
@@ -10,8 +12,14 @@ if (tags.currentKit && tags.currentKit != 'log' && tags.currentKit != 'catalog')
         phys_kit = getBot('kitId', tags.currentKit);
     }
 
+    let radius = inMap ? .0002 : 2;
+    const angle = Math.random() * Math.PI * 2;
+    const distance = Math.sqrt(Math.random()) * radius;
+    const phys_kit_posX = posX + distance * Math.cos(angle);
+    const phys_kit_posY = posY + distance * Math.sin(angle);
+
     if (phys_kit) {
-        phys_kit.manifest({dimension: dimension, position: {x: posX, y: posY + (inMap ? .0002 : 2)}})
+        phys_kit.manifest({dimension: dimension, position: {x: phys_kit_posX, y: phys_kit_posY}})
     }
 }
 
@@ -34,8 +42,10 @@ if (!links.kitBot) {
 
 if (links.kitBot) {
     links.kitBot.tags[dimension] = false;
+    newPosX = links.kitBot.tags[dimension + 'X'];
+    newPosY = links.kitBot.tags[dimension + 'Y'];
 }
 
 destroy(links.abBot);
 
-thisBot.abManifestBot({...that, dimension: dimension, position: {x: posX, y: posY}});
+thisBot.abManifestBot({...that, dimension: dimension, position: {x: newPosX, y: newPosY}});

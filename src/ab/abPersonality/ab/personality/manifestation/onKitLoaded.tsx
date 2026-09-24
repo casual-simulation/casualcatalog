e1e18702eec764b4b9ce13dd2b60b27df8a07f92
Elise Reynolds <elise.reynolds@yeticgi.com>
2026-09-24 16:07:12 -0400

    [abPersonality]: fixing typo

--- src/ab/abPersonality/ab/personality/manifestation/onKitLoaded.tsx
+++ src/ab/abPersonality/ab/personality/manifestation/onKitLoaded.tsx
@@ -1,15 +1,12 @@
 console.log("kit loaded", that, that.tags.kitId, tags.currentKit, links.abBot, masks.manifestingAB);
-if (that.tags.kidId == tags.currentKit) {
-    console.log("kit check 1");
+if (that.tags.kitId == tags.currentKit) {
 
     if (masks.manifestingAB == true) {
-        console.log("kit check 2");
         masks.awaitingManifestation = true;
         return;
     }
 
     if (links.abBot) {
-        console.log("found abBot")
         const dimension = configBot.tags.mapPortal ?? configBot.tags.gridPortal;
         thisBot.abManifestBot({
             dimension: dimension,
